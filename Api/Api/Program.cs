a2b0676721e4a37b2904dba2f60be043f248b2a1
Richard Sobreiro <richardsobreiro@gmail.com>
2023-04-10 17:42:00 -0300

    FIT: add app.MapHealthChecks

--- Api/Api/Program.cs
+++ Api/Api/Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddHealthChecks();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -19,5 +19,7 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapHealthChecks("/healthz");
 
 app.Run();
